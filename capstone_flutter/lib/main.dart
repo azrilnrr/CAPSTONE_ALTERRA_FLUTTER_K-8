a416cc5d2c4185f24c271232ac01fcc18c65cc6f
Anggundini17 <anggundiniwahyuni@gmail.com>
2023-05-17 02:11:50 +0700

    Coding kasaran layout halaman homepage

--- capstone_flutter/lib/main.dart
+++ capstone_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:capstone_flutter/view/home.dart';
 import 'package:capstone_flutter/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      // home: LoginScreen(),
+      home: HomePageScreen(),
     );
   }
 }
